2ce0346c082c45aa4e5045ef25df5aa5882340bd
Jonathan Land <land.jonathan@gmail.com>
2021-03-07 17:37:57 +0200

    feat(math): inRange

--- functions/math.mjs
+++ functions/math.mjs
@@ -9,3 +9,12 @@
  * @return {number}
  */
 export const mapRange = (value, originalStart = 0, originalEnd = 1, newStart = 0, newEnd = 100) => (value - originalStart) * (newEnd - newStart) / (originalEnd - originalStart) + newStart
+
+/**
+ * Determines whether a number is between a min and max
+ * @param {number} x
+ * @param {number} min
+ * @param {number} max
+ * @return {boolean}
+ */
+export const inRange = (x, min, max) => ((x-min)*(x-max) <= 0)
